ea27a7f3100cb1c639539f91cd14ca0aab9fc515
Katya Atamanchuk <ua.katerynka@gmail.com>
2014-11-23 22:33:49 +0200

    auth moved to controller

--- src/controller/client/LoginController.java
+++ src/controller/client/LoginController.java
@@ -4,7 +4,13 @@ import shared.model.dao.impl.UserDAOImpl;
 import shared.model.vo.User;
 
 public class LoginController {
-    public static User getUserObject(String login) {
-        return new UserDAOImpl().getUserByLogin(login);
+    public static User getUserObject(String login, String password) {
+        User user = new UserDAOImpl().getUserByLogin(login);
+        if (user!=null && user.getPassword().equals(password)) {
+            return user;
+        } else {
+            return null;
+        }
+
     }
 }
--- src/shared/view/desktop/MainWindow.java
+++ src/shared/view/desktop/MainWindow.java
@@ -30,8 +30,8 @@ public abstract class MainWindow {
 
             String username = userText.getText();
             String password = new String(passwordText.getPassword());
-            User user = LoginController.getUserObject(username);
-            if (user != null && user.getPassword().equals(password)) {
+            User user = LoginController.getUserObject(username, password);
+            if (user != null) {
                 if (user.isAdmin()) {
                     placeAdminComponents();
                 } else {
